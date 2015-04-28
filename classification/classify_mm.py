55d7e82a7d602d1fad7d6895c394de11043de621
Jack Lot <Jacklotkowski@gmail.com>
2015-04-27 20:13:59 -0400

    Fixed some of the tweet file names to utf-8 encoding with no special weird characters

--- classification/classify_mm.py
+++ classification/classify_mm.py
@@ -1,4 +1,5 @@
 from metamind.api import set_api_key, twitter_text_classifier, ClassificationData, ClassificationModel, set_api_key
+import os, re
 
 #Set the metamind API key for my account
 set_api_key("IpdP8N0nsPmYstaqwqL1CWpPWfxxETCj5BzQWa7ANN6ChZ9PYS")
@@ -6,7 +7,29 @@ set_api_key("IpdP8N0nsPmYstaqwqL1CWpPWfxxETCj5BzQWa7ANN6ChZ9PYS")
 #Load the classifier we trained on the 3 class Rotten Tomatoes movie reviews
 classifier = ClassificationModel(id=25412)
 
-tsv_tweets_out = "datasets/tweets/"
+#Define the file paths to our data
+tsv_tweets = "datasets/tweets/"
+tweets_out = "datasets/tweets_out/"
 tweet_types = ["good", "bad", "recent"]
 
-print classifier.predict("datasets/tweets/good/The_Imitation_Game.tsv", input_type="tsv")
+#Loop through each folder of good, bad and recent tweets and run
+#the classifier on it
+for type in tweet_types:
+
+	rel_path = tsv_tweets + type + "/"
+	rel_out_path = tweets_out + type + "/"
+
+	#Loop through each movie tweet file
+	for movie_tweets_file in os.listdir(rel_path):
+
+		movie_name = movie_tweets_file[:-4]
+
+		print "Predicting... " + rel_path + movie_tweets_file
+
+		#Write the output to a new file
+		f = open(rel_out_path + movie_name + ".json", 'w')
+		print >>f, classifier.predict(rel_path + movie_tweets_file, input_type="tsv")
+		f.close()
+
+		print "  Done!"
+		
---
+++ classification/wrangle_tweets.py
@@ -0,0 +1,47 @@
+import csv, json, re, os
+
+#Remove irrelevant characters from a string
+def clean(s):
+  return " ".join(re.findall(r'\w+', s, flags = re.UNICODE | re.LOCALE)).lower()
+
+#Creates MetaMind formatted file from tsv file
+def to_mm_tsv(input_file, output_file):
+
+  print "\nReading json from:", input_file,"\nWriting to:", output_file
+
+  f = open(input_file)
+  tweets = json.load(f)
+
+  with open(output_file, "wb") as outfile:
+
+    #Loop through every line of the input file
+    for tweet in tweets:
+
+      tweet_clean = "-1" + "\t" + clean(tweet["tweet_text"])
+      #tweet["tweet_text"].encode('utf-8')
+     
+      #break
+      
+      outfile.write(tweet_clean + "\n")
+
+
+#to_mm_tsv(json_raw_tweets+"good/tweets/The_Imitation_Game_tweets.json", tsv_tweets_out+"good/The_Imitation_Game.tsv")
+
+#Define the file paths to our data
+json_raw_tweets = "../twitter-support/data_sets/"
+tsv_tweets_out = "datasets/tweets/"
+tweet_types = ["good", "bad", "recent"]
+
+#Loop through each folder of good, bad and recent tweets and format
+#the data for use by MetaMind
+for type in tweet_types:
+
+  rel_path = json_raw_tweets + type + "/tweets/"
+  rel_out_path = tsv_tweets_out + type + "/"
+
+  #Loop through each movie tweet file
+  for movie_tweets_file in os.listdir(rel_path):
+
+    movie_name = movie_tweets_file#[:-5]
+    print type + " " + movie_name[-5:]
+    #to_mm_tsv(rel_path + movie_tweets_file, rel_out_path + movie_name + ".tsv")
