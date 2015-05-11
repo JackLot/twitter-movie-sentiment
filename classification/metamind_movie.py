f4f7cb77f4fb93f01a9bb542dbdf06ffa8d6d49d
Jack Lot <Jacklotkowski@gmail.com>
2015-05-11 12:55:01 -0400

    Removed some unused classification files

--- classification/metamind_movie.py
+++ classification/metamind_movie.py
@@ -2,10 +2,6 @@ from metamind.api import set_api_key, twitter_text_classifier, ClassificationDat
 
 set_api_key("5eqwiKI50ym253djlf84VEgQptIb5odohKFpgS1SSWOdeGDzQ3")
 
-#Using the MetaMind API we can look up things 
-#print twitter_text_classifier.query_and_predict("comcast")
-
-# 
 training_data = ClassificationData(private=True, data_type="text", name="RT snippets training data")
 training_data.add_samples()
 
